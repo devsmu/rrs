8d5bde12ae95b62fdef5615558140d1d8f2754a1
dev.lsh <dev.lsh@gamil.com>
2024-11-26 02:17:21 +0900

    맛집에 등록된 리뷰 가져오기 API 추가

--- src/main/java/com/jyujyu/review/service/ReviewService.java
+++ src/main/java/com/jyujyu/review/service/ReviewService.java
@@ -3,10 +3,12 @@ package com.jyujyu.review.service;
 import com.jyujyu.review.model.ReviewEntity;
 import com.jyujyu.review.repository.RestaurantRepository;
 import com.jyujyu.review.repository.ReviewRepository;
+import com.jyujyu.review.service.dto.ReviewDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.time.ZonedDateTime;
 
@@ -35,5 +37,20 @@ public class ReviewService {
         ReviewEntity review = reviewRepository.findById(reviewId).orElseThrow();
 
         reviewRepository.delete(review);
+    }
+
+    public ReviewDto getRestaurantReview(Long restaurantId, Pageable page){
+        Double avgScore = reviewRepository.getAvgScoreByRestaurantId(restaurantId);
+        Slice<ReviewEntity> reviews = reviewRepository.findSliceByRestaurantId(restaurantId, page);
+
+        return ReviewDto.builder()
+                .avgScore(avgScore)
+                .reviews(reviews.getContent())
+                .page(
+                        ReviewDto.ReviewDtoPage.builder()
+                                .offset(page.getPageNumber() * page.getPageSize())
+                                .build()
+                )
+                .build();
     }
 }
